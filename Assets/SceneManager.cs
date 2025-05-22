8dd0a26b1d448aac5a1540d884043e0ed7e64be4
Rick <rick-schloemilch@web.de>
2025-05-22 23:01:55 +0200

    Menu Scene

---
+++ Assets/SceneManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    [SerializeField] private Camera menuCamera;
+    [SerializeField] private Camera gameCamera;
+    [SerializeField] private GameObject gameUI;
+    [SerializeField] private GameObject menuUI;
+
+    private void Start()
+    {
+        Time.timeScale = 0f;
+        menuCamera.gameObject.SetActive(true);
+        gameCamera.gameObject.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        Time.timeScale = 1f;
+        menuCamera.gameObject.SetActive(false);
+        gameCamera.gameObject.SetActive(true);
+        gameUI.SetActive(true);
+        menuUI.SetActive(false);
+    }
+}
